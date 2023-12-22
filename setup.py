0d0d37c8288f3cc64c78033725ffbcbf1c32f473
Decee1 <48594001+Decee1@users.noreply.github.com>
2023-12-22 12:30:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
+
 setup(name='GPIBPrologix',
-version='0.1',
-description='USB to GPIB Python driver',
-url='#',
-author='Decee1',
-author_email='n.a',
-license='',
-packages=['GPIBPrologix'],
-zip_safe=False)
+      version='0.1',
+      description='USB to GPIB Python driver',
+      url='#',
+      author='Decee1',
+      author_email='n.a',
+      license='',
+      packages=['GPIBPrologix'],
+      zip_safe=False,
+      requires=["pyserial"]
+      )
